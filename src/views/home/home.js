e7108e5c421e50502cb467e70a8d153307911f0b
sunyh1 <sunyh1@guahao.com>
2020-03-31 22:46:16 +0800

    fix: 组件调整

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -5,40 +5,12 @@ import './home.less';
 
 import Markdown from '@/libs/markdown';
 
-import Switch from '@/packages/switch';
-import Button from '@/packages/button';
-import Toast from '@/packages/toast';
-import MessageBox from '@/packages/messagebox';
-
 export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
       checked: false,
     };
-  }
-  
-  install = () => {
-    let str = `
-      import Nerv from 'nervjs';
-      import {Button} from 'pigs-ui';
-      Nerv.render(<Button type="primary">按钮</Button>, document.getElementById('app'))
-    `;
-    return str.split('\n').map(item => item.trim()).join('\n');
-  }
-
-  switchChange = (val) => {
-    console.log(val)
-    this.setState({ checked: val });
-  }
-
-  handleClick = () => {
-    // Toast.info('asdf', 1000);
-    MessageBox.msgbox('title', 'content', 'content2').then(() => {
-      console.log('confirm');
-    }).catch(() => {
-      console.log('cancel');
-    })
   }
 
   render() {
@@ -49,11 +21,6 @@ export default class Home extends Component {
         <pre className="content">
           <code>npm i pigs-ui --S</code>
         </pre>
-
-        <Switch value={this.state.checked} onChange={(val) => this.switchChange(val)}></Switch>
-
-        <Button onClick={() => this.handleClick()}>test</Button>
-
         <p className="title">使用</p>
         <pre className="content">
           <code>
